8788fb57db61a05c3d77e6ac275f1d0e597ccf6b
Gui Ambros <nospamGitHub@ambros.com.br>
2025-12-07 12:32:40 -0500

    Day 7 refactor

--- day7/day7.py
+++ day7/day7.py
@@ -13,18 +13,18 @@ def solution_p1(map):  # pylint: disable=all
                 S[y][x] = 1
                 continue
 
-            # p2: if empty space OR entering the splitter, with a beam coming through, AND the space immediately
+            # p2: if empty space OR entering the splitter, with a beam coming through, AND the position immediately
             # above is NOT a splitter, then carry on the strength of the beam
             if (map[y][x] == "." or map[y][x] == "^") and y >= 1 and (x in B) and map[y - 1][x] != "^":
                 S[y][x] += S[y - 1][x]
 
-            # p1 and p2: if entering the splitter, with a beam coming through, then split
+            # if splitter, and there's a beam coming through, then split into 2 active beams
             if map[y][x] == "^" and (x in B):
                 B.remove(x)
                 num_splits += 1
                 B.add(x - 1)
                 B.add(x + 1)
-                # p2: if splitting, then carry over the strength of the beam
+                # p2: if splitting, carry over the strength of the beam
                 S[y + 1][x - 1] += S[y][x]
                 S[y + 1][x + 1] += S[y][x]
 
